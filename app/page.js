6ee4fad81181763e5f58572fdb92424c30ee000d
pratik_codes <66104012+pratik123-coder@users.noreply.github.com>
2024-07-02 01:14:09 +0530

    fix: landing page

--- app/page.js
+++ app/page.js
@@ -1,17 +1,54 @@
-import { GlobeComponent } from "@/components/globe";
-
+import Image from "next/image";
+import logo from "../public/logo.png";
+import ok from "../public/ok.png";
+import { Instagram, Facebook, Linkedin, Twitter } from 'lucide-react';
 
 export default function Home() {
+  const iconSize = 30;
   return (
-    <div className="bg-gradient-main w-full overflow-hidden text-white">
-    <div className="w-full bg-[url(https://res.cloudinary.com/de7hlbfce/image/upload/v1718923224/KIITMUN2024/fq6jtunghljsjkusazwi.webp)] md:bg-contain bg-cover bg-center bg-no-repeat">
-        <div className="z-100 h-screen flex flex-col items-center justify-center gap-y-8 ">
-            
-            <GlobeComponent />
-            
-            
+    <div className="w-full h-screen relative overflow-hidden">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        src="https://res.cloudinary.com/dbzo04n4l/video/upload/v1719860750/gv4f55yjokhcjywzpxbh.mp4"
+        autoPlay
+        loop
+        muted
+      />
+      <div className="relative z-10 h-full p-6 flex flex-col justify-center items-center space-y-8 max-w-screen-xl mx-auto">
+        <Image src={logo} alt="logo" width={250} height={250} className="pt-12 lg:pt-20 h-50 w-40 lg:w-52" />
+        <Image src={ok} alt="ok" width={1000} height={200} className="animate-pulse" />
+        <p className="text-sm md:text-lg text-center leading-relaxed md:leading-loose px-4">
+          In a world yearning for unity, the <span>12th edition of KIIT International MUN</span> is set to be the most thrilling yet! Join passionate delegates from around the globe in <span className="underline">electrifying debates</span> and groundbreaking collaborations.
+          <br className="md:hidden" />
+          This isn’t just a conference; it’s your chance to make a lasting impact. Feel the <span className="italic">adrenaline</span>, embrace the challenge, and leave your mark.
+          <br />
+          The world is watching—are you ready?
+          <br />
+          <span className="font-bold">Get ready for an unforgettable KIITMUN experience!</span> Stay tuned and keep an eye on our social media handles for updates, exclusive insights, and more exciting announcements. Don’t miss out on the opportunity to be part of something extraordinary.
+        </p>
+        <div className="flex space-x-4">
+          <a href="https://www.instagram.com/instakiitmun/" target="_blank" rel="noreferrer">
+            <div className="p-2 rounded-full bg-gradient-to-r from-purple-500 to-pink-500">
+              <Instagram size={iconSize} />
+            </div>
+          </a>
+          <a href="https://www.facebook.com/kiitmun" target="_blank" rel="noreferrer">
+            <div className="p-2 rounded-full bg-blue-600">
+              <Facebook size={iconSize} />
+            </div>
+          </a>
+          <a href="https://www.linkedin.com/in/kiitmun/" target="_blank" rel="noreferrer">
+            <div className="p-2 rounded-full bg-slate-700">
+              <Linkedin size={iconSize} />
+            </div>
+          </a>
+          <a href="https://twitter.com/kiitmun" target="_blank" rel="noreferrer">
+            <div className="p-2 rounded-full bg-blue-500">
+              <Twitter size={iconSize} />
+            </div>
+          </a>
         </div>
-    </div>
+      </div>
     </div>
   );
 }
